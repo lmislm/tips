99f99482e83b40fbbe20edd7821d2d091c343c27
lmislm <wszdlm@gmail.com>
2021-01-25 09:56:43 +0800

    [feat] (lc) add leetcode 100.

---
+++ leetcode/[10]_100.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * 100. 相同的树
+ * https://leetcode-cn.com/problems/same-tree/
+ * 
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+	if (p === null && q === null) return true
+	// 判空
+	if (p === null || q === null) return false
+	// 比较相等
+	if (p !== null && q !== null && p.val !== q.val) return false
+	// 递归
+	return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+};

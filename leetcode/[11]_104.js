c88ef925780e9797bc72e7b10caf14424998412b
lmislm <wszdlm@gmail.com>
2021-01-25 17:47:13 +0800

    [feat] (lc) add leetcode accepted problem

---
+++ leetcode/[11]_104.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * 104. 二叉树的最大深度
+ * https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (root == null) return root
+  // 递归
+  const lHeight = maxDepth(root.left)
+  const rHeight = maxDepth(root.right)
+  return Math.max(lHeight, rHeight) + 1
+};

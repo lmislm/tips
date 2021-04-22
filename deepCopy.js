2054e5e17a3ee24d23acde334dd40c169020290a
lmislm <wszdlm@gmail.com>
2021-04-22 11:37:13 +0800

    feat(lib): add deepCopy js

--- deepCopy.js
+++ deepCopy.js
@@ -3,3 +3,21 @@ JSON.parse(JSON.stringify(obj))
 // ES6
 Object.assign({}, obj)
 {...obj}
+// deepClone
+function deepClone (val) {
+	const isPlainObject = (val) => {
+		return Object.prototype.toString.call(val) === '[object object]'
+	}
+	if (isPlainObject(val)) {
+		const res = {}
+		for (const key in val) {
+			res[key] = deepClone(val[key])
+		}
+		return res
+	} else if (Array.isArray(val)) {
+		return val.slice()
+	} else {
+		return val
+	}
+}
+// deepClone 消除循环引用

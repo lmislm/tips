5b27c57af92cb9ea5ee7110d89c1c29ad2f64227
lmislm <wszdlm@gmail.com>
2021-08-21 20:04:21 +0800

    feat: update lib

--- extend.js
+++ extend.js
@@ -1,67 +1,70 @@
-// // call继承
-// // function Parent() {
-// // }
-// // function Child () {
-// //   Parent.call(this)
-// // }
+// call继承
+// function Parent() {
+// }
+// function Child () {
+//   Parent.call(this)
+// }
 
-// // prototype的继承
-// // function Parent() {}
-// // function Child () {}
-// // Child.prototype = new Parent()
+// prototype的继承
+// function Parent() {}
+// function Child () {}
+// Child.prototype = new Parent()
 
-// // 组合继承
-// // function Parent () {}
-// // function Child () {
-// //   Parent.call(this)
-// // }
-// // Child.prototype = new Parent()
-
-// // 寄生组合继承，组合继承的优化
+// 组合继承
 // function Parent () {}
 // function Child () {
 //   Parent.call(this)
 // }
-// Child.prototype = Object.create(Parent.prototype)
-// Child.prototype.constructor = Child
+// Child.prototype = new Parent()
 
-// // 原型式继承
-// function object(o) {
-//   function F() {}
-//   F.prototype = o
-//   return new F()
+// 寄生组合继承，组合继承的优化
+function Parent() {}
+function Child() {
+  Parent.call(this);
+}
+Child.prototype = Object.create(Parent.prototype);
+Child.prototype.constructor = Child;
+/**
+ * 寄生组合继承，范例
+ * @param {*} name
+ */
+function Person(name) {
+  this.name = name;
+}
+Person.prototype.sayName = function () {
+  console.log("My name is " + this.name);
+};
+
+function Student(name, grade) {
+  Person.call(this, name);
+  this.grade = grade;
+}
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+Student.prototype.sayMyGrade = function () {
+  console.log("My grade is" + this.grade);
+};
+
+// 原型式继承
+function object(o) {
+  function F() {}
+  F.prototype = o;
+  return new F();
+}
+// function Bromise () {
+//   var self = this
+
+//   self.status = 'pending'
+//   self.data = undefined
+//   self.onResolvedCallback = []
+//   self.onRejectedCallback = []
+
 // }
-// // function Bromise () {
-// //   var self = this
 
-// //   self.status = 'pending'
-// //   self.data = undefined
-// //   self.onResolvedCallback = []
-// //   self.onRejectedCallback = []
+// Bromise.prototype.then = function (resolved, rejected) {
 
-// // }
+// }
 
-// // Bromise.prototype.then = function (resolved, rejected) {
+// funtion resolvePromise() {
 
-// // }
-
-// // funtion resolvePromise() {
-
-// // }
-
-let r = [
-  {
-    attrName: '粘稠度',
-    attrValue: '5W-30'
-  },
-  {
-    attrName: '规格',
-    attrValue: '4L'
-  }
-]
-
-r = r.map(item => {
-  return item.attrName + '：' + item.attrValue
-}).join('；')
-
-console.log(r)
+// }
